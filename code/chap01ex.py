93449c069d0bca3ef23e1f2aefe762df6904837f
erika-e <erika.swartz@gmail.com>
2020-07-17 21:29:52 -0400

    Add initial solution code
    Since import nsfg is in the starter file, I'm inferring that I'm not
    supposed to recreate it

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,6 +13,18 @@ import sys
 import nsfg
 import thinkstats2
 
+"""
+#1 write a function that reads the respondent file, 2002FemResp.dat.gz.
+
+The variable pregnum is a recode that indicates how many times each respondent has been pregnant
+
+#2 Print the value counts and comepare them to the codebook
+#3 Cross-validate the respondent and pregnancy files by 
+comparing pregnum with the number of records in the pregnancy file
+
+use nsfg.MakePegMap to make a dictionary that maps from each caseid
+to a list of indices in the dataframe"""
+
 
 def main(script):
     """Tests the functions in this module.
@@ -24,3 +36,7 @@ def main(script):
 
 if __name__ == '__main__':
     main(*sys.argv)
+
+    resp = nsfg.ReadFemResp()
+
+    print(resp.pregnum.value_counts().sort_index())
